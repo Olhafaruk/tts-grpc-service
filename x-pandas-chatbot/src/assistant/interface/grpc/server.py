b1c427000f1c3d50223738ac76876d0e1f32fc96
Olha Faruk <farukolga2017@gmail.com>
2025-08-26 16:16:27 +0200

    feat: add gRPC server skeleton

--- x-pandas-chatbot/src/assistant/interface/grpc/server.py
+++ x-pandas-chatbot/src/assistant/interface/grpc/server.py
@@ -1,2 +1,37 @@
 #interface/grpc/server.py
 
+import grpc
+from concurrent import futures
+from assistant.interface.grpc import assistant_pb2, assistant_pb2_grpc
+from assistant.application.index_service import IndexService
+from assistant.application.query_service import QueryService
+from assistant.domain.question import Question
+
+class AssistantServicer(assistant_pb2_grpc.AssistantServicer):
+    def __init__(self):
+        self.indexer = IndexService()
+        self.querier = QueryService()
+
+    def UploadTable(self, request, context):
+        table = self.indexer.upload_table(request.name, request.csv)
+        return assistant_pb2.UploadTableRes(table_id=table.id)
+
+    def Ask(self, request, context):
+        tables = [ self.indexer.upload_table("tmp", b"") ]  # позже замените на кэш
+        for part in self.querier.ask(tables, Question(request.question)):
+            if "code" in part:
+                yield assistant_pb2.AskRes(code=assistant_pb2.Code(content=part["code"]))
+            elif "log" in part:
+                yield assistant_pb2.AskRes(log=assistant_pb2.Log(line=part["log"]))
+            else:
+                yield assistant_pb2.AskRes(text=assistant_pb2.Text(msg=part["text"]))
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+    assistant_pb2_grpc.add_AssistantServicer_to_server(AssistantServicer(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
